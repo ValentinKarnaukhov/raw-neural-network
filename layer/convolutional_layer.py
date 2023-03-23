7b9ca57a459a678ecd32c86fe33ed236ae03cdb5
Valentin <valentine.karnaukhov@gmail.com>
2023-03-23 14:34:40 +0200

    padding for input data

--- layer/convolutional_layer.py
+++ layer/convolutional_layer.py
@@ -5,9 +5,9 @@ from layer.layer import Layer
 
 class ConvolutionalLayer(Layer):
 
-    # kernel_shape - (n, m, k), n - number, m x k - size
-    # input_shape - (p, q, l), l - channels, p x q - size
-    # weights - (n, m, k, l)
+    # kernel_shape - dimensions (n, m, k), n - amount of filters, m x k - size of filter
+    # input_shape - dimensions (p, q, l), l - amount of input channels, p x q - size
+    # weights - dimensions (n, m, k, l), n - amount of filters, m x k - size of filter, l - kernels per filter
     def __init__(self, kernel_shape, input_shape):
         self.kernel_shape = kernel_shape
         self.input_shape = input_shape
@@ -20,34 +20,28 @@ class ConvolutionalLayer(Layer):
 
     def forward_propagation(self, input_data):
         self.output = np.zeros(self.output_shape)
-        self.input = np.pad(input_data, [(self.up_bottom_padding, self.up_bottom_padding),
+        self.input = input_data
+        padded_input = np.pad(input_data, [(self.up_bottom_padding, self.up_bottom_padding),
                                          (self.left_right_padding, self.left_right_padding), (0, 0)])
 
         for kernel_index in range(self.kernel_shape[0]):
-            for i in range(self.input.shape[0] - self.kernel_shape[1] + 1):
-                for j in range(self.input.shape[1] - self.kernel_shape[2] + 1):
-                    receptive_field = self.input[i:i + self.kernel_shape[1], j:j + self.kernel_shape[2], :]
+            for i in range(padded_input.shape[0] - self.kernel_shape[1] + 1):
+                for j in range(padded_input.shape[1] - self.kernel_shape[2] + 1):
+                    receptive_field = padded_input[i:i + self.kernel_shape[1], j:j + self.kernel_shape[2], :]
                     convolved_values = np.multiply(receptive_field, self.weights[kernel_index])
                     self.output[i, j, kernel_index] += np.sum(convolved_values)
         return self.output
 
     def backward_propagation(self, output_error, learning_rate):
-        input_error = np.zeros(self.input.shape)
-        padded_output_error = np.pad(output_error, [(self.up_bottom_padding, self.up_bottom_padding),
-                                                    (self.left_right_padding, self.left_right_padding), (0, 0)])
-        rotated_weights = np.flip(self.weights, axis=(1, 2))
+        input_error = np.zeros(self.input_shape)
+        padded_input = np.pad(self.input, [(self.up_bottom_padding, self.up_bottom_padding),
+                                           (self.left_right_padding, self.left_right_padding), (0, 0)])
 
         for kernel_index in range(self.kernel_shape[0]):
-            for i in range(self.output.shape[0] - self.kernel_shape[1] + 1):
-                for j in range(self.output.shape[1] - self.kernel_shape[2] + 1):
-                    output_error_val = padded_output_error[i:i + self.kernel_shape[1],
-                                       j:j + self.kernel_shape[2], kernel_index]
-                    input_error[i:i + self.kernel_shape[1], j:j + self.kernel_shape[2], :] += (
-                            output_error_val[:, :, np.newaxis] * rotated_weights[kernel_index])
-                    self.weights[kernel_index] -= (
-                            learning_rate * np.multiply(output_error_val[:, :, np.newaxis],
-                                                        self.input[i:i + self.kernel_shape[1],
-                                                        j:j + self.kernel_shape[2], :])
-                    )
+            for i in range(padded_input.shape[0] - self.kernel_shape[1] + 1):
+                for j in range(padded_input.shape[1] - self.kernel_shape[2] + 1):
+                    input_error[i:i + self.kernel_shape[1], j:j + self.kernel_shape[2], :] += \
+                        self.weights[kernel_index] * output_error[i, j]
+
 
         return input_error
